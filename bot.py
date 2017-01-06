81518de072d91a4f01a079beb68a334a70812f7d
pseguel <pseguel@cisco.com>
2017-01-06 00:16:19 -0300

    version inicial

---
+++ bot.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from builtins import object
+
+import json
+import web
+import requests
+
+from ciscosparkapi import CiscoSparkAPI, Webhook
+
+# Global variables
+urls = ('/sparkwebhook', 'webhook')
+app = web.application(urls, globals())
+api = CiscoSparkAPI()
+
+
+class webhook(object):
+         def POST(self):
+            #atiende al POST de Spark
+            json_data = web.data()
+
+            webhook_obj = Webhook(json_data)
+            room = api.rooms.get(webhook_obj.data.roomId)
+            message = api.messages.get(webhook_obj.data.id)
+            person = api.people.get(message.personId)
+
+            # para no responderme a mi mismo
+            me = api.people.me()
+            if message.personId == me.id:
+                return 'OK'
+            else:
+                if 'hola' in message.text:
+                    answer = 'hola amigo'
+                    response_message = api.messages.create(room.id,
+                                                           text=answer)
+            return 'OK'
+
+if __name__ == '__main__':
+    app.run()
